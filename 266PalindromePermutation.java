78affa40ad34fdd6a87c96fd4cc234e25bed0e05
unknown <yue@AVS5000-VS02.aristeiacapital.com>
2016-01-26 19:22:09 -0500

    new commit

--- 266PalindromePermutation.java
+++ 266PalindromePermutation.java
@@ -6,6 +6,16 @@ For example,
 
 public class 266PalindromePermutation {
     public boolean canPermutePalindrome(String s) {
-
+    	if(s==null)
+    		return false;
+    	HashSet<Character> set = new HashSet<Character>();
+    	for(int i = 0;i<s.length();i++){
+    		char c = s.charAt(i);
+    		if(!set.contains(c))
+    			set.add(c);
+    		else
+    			set.remove(c);
+    	} 
+    	return (set.size()==1||set.size()==0);
     }
-}
+}	
--- 293FlipGame.java
+++ 293FlipGame.java
@@ -1,5 +1,9 @@
 /*
-You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+You are playing the following Flip Game with your friend: Given a string that contains only these 
+
+two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". 
+
+The game ends when a person can no longer make a move and therefore the other person will be the winner.
 
 Write a function to compute all possible states of the string after one valid move.
 
@@ -14,3 +18,17 @@ For example, given s = "++++", after one move, it may become one of the followin
 
 If there is no valid move, return an empty list [].
 */
+
+public class 293FlipGame{
+	public ArrayList<String> getAllPossibleStates(String s){
+		ArrayList<String> list = new ArrayList<String>(); 
+		if(s==null||s.length()==0||s.length()==1)
+			return list; 
+		for(int i=0;i<s.length()-1;i++){
+			if(s.substring(i,i+2).equals("++"))
+				list.add(s.substring(0,i)+s.substring(i).replaceFirst("++","--"));
+		}
+		return list;
+	}
+
+}
