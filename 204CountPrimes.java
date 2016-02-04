23bb01563aa703a185885d32538ca5a90b1810a3
Stacy <shenyue.2010@gmail.com>
2016-02-04 18:04:30 -0500

    new

--- 204CountPrimes.java
+++ 204CountPrimes.java
@@ -6,7 +6,12 @@ Description:
 Count the number of prime numbers less than a non-negative number, n.
 */
 public class 204CountPrimes  {
+	//if n-1 is prime, countPrimes(n) = countPrimes(n-1)+1;
+	//if n-1 isnot prime, countPrimes(n) = countPrimes(n-1);
     public int countPrimes(int n) {
         
+    }
+    private boolean isPrime(int n){
+
     }
 }
