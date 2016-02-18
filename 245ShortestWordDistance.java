58b84c77005f7c90a86c292b6c3c3be3a339f8d1
Stacy Shen <shenyue.2010@gmail.com>
2016-02-17 21:47:08 -0500

    new

--- 245ShortestWordDistance.java
+++ 245ShortestWordDistance.java
@@ -13,5 +13,22 @@ You may assume word1 and word2 are both in the list.
 */
 public class 245ShortestWordDistance{
 	public int getShortestWordDistance(String[] list, String w1, String w2){
+		int i1=-1;
+		int i2=-1;
+		int d = Math.MAX_VALUE;
+		for(int i=0;i<list.length;i++){
+			if(w1.euqals(w2)&&i1!=i2)
+				i2=i1;
+			if(w1.euqals(list[i]))
+				i1=i;
+			else if(w2.equals(list[i]))
+				i2=i;
+			else
+				 continue;
+			if(i1!=-1&&i2!=-1&&i1!=i2&&d>Math.abs(i2-i1))
+				d = Math.abs(i2-i1);
+
+		}
+		return d;
 	}
 }
--- 311SparseMatrixMultiplication.java
+++ 311SparseMatrixMultiplication.java
@@ -21,6 +21,37 @@ AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
 */
 
 public class 311SparseMatrixMultiplication {
+
     public int[][] multiply(int[][] A, int[][] B) {
+    	if(A==null||B==null)
+    		return null;
+    	int[][] result = new int[A.length][B[0].length];//here A.length==B[0].length
+    	int col = 0;
+    	for(int i=0;i<A.length;){
+    		int sum = 0;
+    		for(int j=0;j<B[0].length;j++)
+    			sum+=A[i][j]*B[j][col];
+    		result[i][col++] = sum;
+    		if(col==A.length){
+    			col=0;
+    			i++;
+    		}
+    	}
+    	return result;
+    }
+    
+    public int[][] multiply2(int[][] A, int[][] B) {
+    	if(A==null||B==null)
+    		return null;
+    	int[][] result = new int[A.length][B[0].length];//here A.length==B[0].length
+    	for(int i=0;i<A.length;){
+    		for(int j=0;j<A[0].length;j++)
+    			if(A[i][j]!=0){
+    				for(int k=0;k<B[0].length;k++)
+ 		   				result[int][k]+=A[i][j]*B[j][k];
+    			}
+    			
+    	}
+    	return result;
     }
 }
