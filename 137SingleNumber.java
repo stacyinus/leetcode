2251817b86025e30b17310f310bc0d44a808f305
Stacy <shenyue.2010@gmail.com>
2016-02-19 18:16:00 -0500

    add new questions

---
+++ 137SingleNumber.java
@@ -0,0 +1,11 @@
+/*
+137. Single Number II 
+
+Given an array of integers, every element appears three times except for one. Find that single one.
+*/
+
+public class 137SingleNumber {
+    public int singleNumber(int[] nums) {
+        
+    }
+}
--- 318MaximumProductOfWordLengths.java
+++ 318MaximumProductOfWordLengths.java
@@ -22,12 +22,25 @@ No such pair of words.
 
 public class 318MaximumProductOfWordLengths {
     public int maxProduct(String[] words) {
-    	HashSet<Character>[] s = new HashSet<Character>[words.length];
+    	HashSet[] s = new HashSet[words.length];
     	for(int i=0;i<words.length;i++){
     		s[i] = new HashSet<Character>();
     		for(int j=0;j<words[i].length();j++)
     			s[i].add(words[i].charAt(j));
     	}
-        
+    	int l = 0;
+    	for(int i=0;i<words.length;i++){
+    		for(int j=i+1;j<words.length;j++){
+    			if(noCommon(s[i],s[j]))
+    				l=Math.max(l,words[i].length()*words[j].length());
+    		}
+    	}
+        return l;
+    }
+    private boolean noCommon(HashSet<Character> set1, HashSet<Character> set2){
+    	for(char c: set1)
+    		if(set2.contains(c))
+    			return false;
+    	return true;
     }
 }
