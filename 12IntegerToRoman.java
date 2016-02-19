f7311e0fb1ba31a68bbc5de0793fcb6cbe5b149f
Stacy <shenyue.2010@gmail.com>
2016-02-19 16:24:21 -0500

    2/19/2016

--- 12IntegerToRoman.java
+++ 12IntegerToRoman.java
@@ -8,22 +8,34 @@ Input is guaranteed to be within the range from 1 to 3999.
 
 public class 12IntegerToRoman {
     public String intToRoman(int num) {
-    	int[] a = new int[]{1000,500,100,50,10,5,1};
-    	String[] b= new String[]{"M","D","C","L","X","V","I"};
-    	int i=0;
-    	String r="";
-    	while(i<a.length-1){
-    		if(num/a[i]==1){
-    			if(num/a[i+1]==9)
-    				r+=b[i+1]+b[i-1];
-    			else
-    				r+=b[i];
-    		}
-    		else if(num/a[i]>1){
-    			
-    		}
-    		i++;
-    	}
-         
+        String[] b= new String[]{"M","D","C","L","X","V","I"};
+        int i=b.length-1;
+        String r="";
+        int d=0;
+        while(num>0&&i>=0){
+            d=num%10;
+            String tmp ="";
+            if (d==9){
+                d-=9;
+                tmp=b[i]+b[i-2];
+            }
+            if(d>=5){
+                d-=5;
+                tmp=b[i-1];
+            }
+            if(d==4){
+                d-=4;
+                tmp+=b[i]+b[i-1];
+            }   
+            if(d<4){
+                for(int j=d;j>0;j--)
+                    tmp+=b[i];                
+            }
+            if(!tmp.equals(""))
+                r=tmp+r;
+            i-=2;
+            num/=10;
+        }
+        return r;
     }
 }
