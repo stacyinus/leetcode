f4fc918faef84a1fdadbacd8d0ea9a1d4208afcf
stacyinus <shenyue.2010@gmail.com>
2016-02-25 18:02:34 -0500

    2/25/2016

--- 259ThreeSumSmaller.java
+++ 259ThreeSumSmaller.java
@@ -1,6 +1,7 @@
 /*
 259. 3Sum Smaller
-Given an array of n integers nums and a target, find the number of index triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target.
+Given an array of n integers nums and a target, find the number of index triplets i, j, k with 0 <= i < j < k < n 
+that satisfy the condition nums[i] + nums[j] + nums[k] < target.
 For example, given nums = [-2, 0, 1, 3], and target = 2.
 Return 2. Because there are two triplets which sums are less than 2:
 [-2, 0, 1]
@@ -11,6 +12,17 @@ Could you solve it in O(n2) runtime?
 
 public class 259ThreeSumSmaller {
     public int threeSumSmaller(int[] nums, int target) {
-
+    	HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+    	for(int i = 0;i<nums.length-1;i++){
+    		for(int j=i+1;j<nums.length;j++){
+    			if(!map.contains(nums[i]+nums[j]))
+    				map.put(nums[i]+nums[j],1);
+    			else
+    				map.put(nums[i]+nums[j],map.get(nums[i]+nums[j])+1);
+    		}
+    	}
+    	for(int i=0;i<nums.length;i++){
+    		if(target-)
+    	}
     }
 }
--- 281ZigzagIterator.java
+++ 281ZigzagIterator.java
@@ -10,4 +10,33 @@ Follow up: What if you are given k 1d vectors? How well can your code be extende
 */
 
 public class ZigzagIterator {
+	List<List<Integer>> lists;
+	int indexOfLists;
+	int[] index;
+	int numOfElementsLeft;
+
+	public ZigzagIterator(List<List<Integer>> lists){
+		this.lists = lists;
+		this.index = new int[lists.size()];
+		for(int i:this.index)
+			this.index[i]=-1;
+		this.indexOfLists = -1;
+		for(List<Integer> l:lists)
+			this.numOfElementsLeft += l.size();
+		
+	}
+
+	public boolean hasNext(){
+		return numOfElementsLeft == 0; 
+	}
+
+	public void next(){
+		if(!hasNext())
+			return;
+		indexOfLists = indexOfLists+1>=lists.size()?indexOfLists+1-lists.size():indexOfLists+1;
+		while(index[indexOfLists]+1>=list.get(indexOfLists).size())
+			indexOfLists = indexOfLists+1>=lists.size()?indexOfLists+1-lists.size():indexOfLists+1;
+		index[indexOfLists] = index[indexOfLists]+1;
+		numOfElementsLeft--;
+	}
 }
