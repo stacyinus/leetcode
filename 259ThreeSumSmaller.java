357ca732e1ed64253b236cf9dd137123cc7a570a
stacyinus <shenyue.2010@gmail.com>
2016-02-26 17:29:34 -0500

    2/26

--- 259ThreeSumSmaller.java
+++ 259ThreeSumSmaller.java
@@ -12,17 +12,18 @@ Could you solve it in O(n2) runtime?
 
 public class 259ThreeSumSmaller {
     public int threeSumSmaller(int[] nums, int target) {
-    	HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-    	for(int i = 0;i<nums.length-1;i++){
-    		for(int j=i+1;j<nums.length;j++){
-    			if(!map.contains(nums[i]+nums[j]))
-    				map.put(nums[i]+nums[j],1);
-    			else
-    				map.put(nums[i]+nums[j],map.get(nums[i]+nums[j])+1);
-    		}
-    	}
-    	for(int i=0;i<nums.length;i++){
-    		if(target-)
-    	}
+        int result = 0;
+        for(int i=0;i<nums.length-2;i++){
+            int j=i+1;
+            int k=sums.length-1;
+            while(j<k){
+                if(nums[i] + nums[j] + nums[k] < target){
+                    result+=k-j;
+                    j++;
+                }
+                else
+                    k--;
+            }
+        }
     }
 }
