af01aae49c7411039a55f6355a5cab78a2b9cf0b
stacyinus <shenyue.2010@gmail.com>
2016-05-03 18:15:08 -0400

    new

---
+++ 23MergeKSortedLists.java
@@ -0,0 +1,19 @@
+/*
+23. Merge k Sorted Lists
+
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+}
--- 81SearchInRotatedSorted Array.java
+++ 81SearchInRotatedSorted Array.java
@@ -10,7 +10,15 @@ Write a function to determine if a given target is in the array.
 */
 
     public class Solution {
+        //if duplicates are allowed, then it is not possible to binary search this problem
+        // think about the extreme cases,seach 1 in an array contain all 2s.
         public boolean search(int[] nums, int target) {
-            
+        	if(nums == null || nums.length == 0)
+        		return false;
+            for(int i = 0; i < nums.length; i++){
+            	if(nums[i] == target)
+            		return true;
+            }
+            return false;
         }
     }
