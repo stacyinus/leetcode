2df3b0d182c6fc6da8240f42408817b787bcd5e1
stacyinus <shenyue.2010@gmail.com>
2016-05-05 18:32:36 -0400

    5/5/2016

--- 179LargestNumber.java
+++ 179LargestNumber.java
@@ -13,19 +13,35 @@ Special thanks to @ts for adding this problem and creating all test cases.
 Subscribe to see which companies asked this question
 */
 public class Solution {
+	// most concise method using new libs in java 8
+    public String largestNumber(int[] num) {
+        String[] array = Arrays.stream(num).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(array, (String s1, String s2) -> (s2 + s1).compareTo(s1 + s2));
+        return Arrays.stream(array).reduce((x, y) -> x.equals("0") ? y : x + y).get();
+    } 
+    //convert numbers to strings and compare.
+	public class StringComparator implements Comparator<String>{
+		public int compare(String a, String b){
+			if(a.length() == b.length())
+				return b.compareTo(a);//Here use b.compareTo(a) not a.compareTo(b) because you want to sort it desc
+			String ab = a + b;
+			String ba = b + a;
+			return ba.compareTo(ab);
+		}
+	}
     public String largestNumber(int[] nums) {
-        
+        ArrayList<String> numStr = new ArrayList<String>(Arrays.asList(nums));
+        Collections.sort(numStr, new StringComparator());
+        StrinbBuilder sb = new StringBuilder();
+        for(String s : numStr)
+        	sb.append(s);
+        String res = sb.toString();
+        if(res.length() > 0 && res.charAt(0) == '0')
+        	return "0";
+        return res;
+
     }
-    private boolean compareTo(int a, int b){
-    	while(a!=0 && b!=0 && a/10 == b/10){
-    		a = a%10;
-    		b = b%10;
-    	}
-    	if(a/10 > b/10)
-    		return 1;
-    	if(a/10 == b/10)
-    		return 0;
-    	if(a/10 < b/10)
-    		return -1;
-    }
-}
+
+
+
+}	
---
+++ 220ContainsDuplicateIII.java
@@ -0,0 +1,13 @@
+/*
+220. Contains Duplicate III  
+
+Given an array of integers, find out whether there are two distinct indices i and j in the array 
+
+such that the difference between nums[i] and nums[j] is at most t and the difference between i and j is at most k.
+*/
+
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        
+    }
+}	
