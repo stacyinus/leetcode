4c60167158856b8376f8d999ba1cb1c6520ff2cd
Stacy Shen <shenyue.2010@gmail.com>
2016-06-14 21:10:19 -0400

    new

--- 62UniquePaths.java
+++ 62UniquePaths.java
@@ -12,22 +12,16 @@ Above is a 3 x 7 grid. How many possible unique paths are there?
 
 Note: m and n will be at most 100.
 
+/*
+
+    DP:p[i][j] represents the total number of paths to a particular grid(i,j). then we know, p[i][j]=p[i-1][j]+p[i][j-1]
+    we also know that p[i][0] =1 because the robot can only go down
+    p[0][j]=1 because the robot can only go right.
+    T: O(m*n) 
+    S: O(m*n) can be optimize
 */
-
 public class Solution {
-	//first thought...but this is too slow
-    public int uniquePaths(int m, int n) {
-    	if(m==0||n==0)
-    		return 0;
-        if(m==1||n==1)
-        	return 1;
-        return uniquePaths(m-1,n)+uniquePaths(m,n-1);       
-    }
 
-    //Second thought:p[i][j] represents the total number of paths to a particular grid(i,j). then we know, p[i][j]=p[i-1][j]+p[i][j-1]
-    // we also know that p[i][0] =1 because the robot can only go down
-    //p[0][j]=1 because the robot can only go right.
-    //T: O(m*n), S: O(m*n)
     public int uniquePaths(int m, int n) {
     	int[][] p = new int[m][n];
     	for(int i=0;i<m;i++)
