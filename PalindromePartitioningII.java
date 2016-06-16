f47f0363d109e0933d837f733dbaaef5d4b98303
stacyinus <shenyue.2010@gmail.com>
2016-06-16 17:57:02 -0400

    6/16/2016

--- PalindromePartitioningII.java
+++ PalindromePartitioningII.java
@@ -11,12 +11,47 @@ Given s = "aab",
 Return 1 since the palindrome partitioning ["aa", "b"] could be produced using 1 cut.
 */
 
+
+/*
+	DP: dp array represents "the minimum cuts needed for a palindrome partitioning for the first
+		i characters". 
+*/
 public class Solution {
     /**
      * @param s a string
      * @return an integer
      */
-    public int minCut(String s) {
+    public int minCut(String s)	 {
         // write your code here
+        boolean[][] isParlindrome = findMatrix(s);
+
+    	int[] dp = new int[s.length() + 1];
+    	dp[0] = -1;
+    	for(int i = 1; i <= s.length();i++)
+    		dp[i] = i - 1;
+    	for(int i = 1;i < s.length(); i++)
+    		for(int j = 0; j < i; j++){
+    			if(isPalindrome(s,j,i))
+    				dp[i+1] = Math.min(dp[i+1], dp[j]+1);
+    		}
+    	return dp(s.length());
     }
-};
+    public boolean isPalindrome(String s, int j, int i){ //inclusive
+    	while(j < i){
+    		if(s.charAt(j) != s.charAt(i))
+    			return false;
+    		j++;
+    		i--;
+    	}
+    	return true;
+    }
+    public boolean[][] findMatrix(String s){
+		boolean[][] isParlindrome = new boolean[s.length()][s.length()];
+    	for(int i = 0; i < s.length(); i++ ){
+    		for(int j = 0; j < s.length(); j++){
+    			if(i == j || isPalindrome(s, i, j))
+
+    		}
+    	}
+    }
+};			
--- TheSmallestDifference.java
+++ TheSmallestDifference.java
@@ -1,10 +1,21 @@
 /*
 The Smallest Difference
 
-Given two array of integers(the first array is array A, the second array is array B), now we are going to find a element in array A which is A[i], and another element in array B which is B[j], so that the difference between A[i] and B[j] (|A[i] - B[j]|) is as small as possible, return their smallest difference.
+Given two array of integers(the first array is array A, the second array is array B),
+now we are going to find a element in array A which is A[i], 
+and another element in array B which is B[j], so that the difference 
+between A[i] and B[j] (|A[i] - B[j]|) is as small as possible, 
+return their smallest difference.
 
 Example
 For example, given array A = [3,6,7,4], B = [2,8,9,3], return 0
+*/
+
+/*
+    Time O(nlogn)
+    Space O(1)
+    Method 1. Sort, and use 2 pointers for each array.
+    Method 2. Sort, and use binary search. 
 */
 
 public class Solution {
@@ -12,7 +23,46 @@ public class Solution {
      * @param A, B: Two integer arrays.
      * @return: Their smallest difference.
      */
+
     public int smallestDifference(int[] A, int[] B) {
-        // write your code here
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int val = Integer.MAX_VALUE;
+        int i = 0, j = 0;
+        while( i < A.length && j < B.length){        
+            if(A[i]>B[j]){
+                val = Math.min(val, A[i] - B[j]);
+                j++;
+            }
+            else{
+                val = Math.min(val, B[j] - A[i]);
+                i++;
+            }
+        }
+        return val;
+    }    
+    public int smallestDifference(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int val = Integer.MAX_VALUE;
+        for(int i = 0; i < A.length; i++)
+        	val = Math.min(val, findSmallestDiff(B,A[i]));
+        return val;
+    }
+    public int findSmallestDiff(int[] a, int v){
+    	int i = 0, j = a.length - 1, m = i + ( j - i )/2;
+    	while( i + 1 < j ){
+    		if(a[m] == v)
+    			return 0;
+    		if(a[m] > v)
+    			j = m;
+    		else
+    			i = m;
+    		m = i + ( j - i )/2;
+    	}
+    	int diff = Math.abs(a[i] - v);
+    	if( diff > Math.abs(a[j] - v))
+    		return Math.abs(a[j] - v);
+    	return diff;
     }
 }
