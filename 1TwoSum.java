4f1c350dee9824d5e35293fa3499a7c8351029b1
stacyinus <shenyue.2010@gmail.com>
2016-07-14 17:26:23 -0400

    new

--- 1TwoSum.java
+++ 1TwoSum.java
@@ -7,45 +7,12 @@ You may assume that each input would have exactly one solution.
 Input: numbers={2, 7, 11, 15}, target=9
 
 Output: index1=1, index2=2*/
+/*
+	HashMap: O(n)
+	Cant use 2 pointers here because it is asking to return the pointer. If you sort, then you lose the original position.
+*/
 
-public class 1TwoSum {     
-	//O(n)
-	public int[] twoSum2(int[] nums, int target) {
-		HashMap<Integer,ArrayList<Integer>> map = new HashMap<Integer,ArrayList<Integer>>();      
-		if(nums == null || nums.length < 2)
-            return null;
-		int[] result = new int[2];        
-		for(int i=0;i<nums.length;i++){
-			ArrayList<Integer>  value;
-			if(!map.containsKey(nums[i]))
-				value = new ArrayList<Integer>();      			
-			else
-				value = map.get(nums[i]);			
-			value.add(i+1);
-			map.put(nums[i], value );
-		}
-		
-		for(int i: nums){
-			if(map.containsKey(target-i)){
-				if(i == (target-i)){
-					if(map.get(i).size()>1){
-						result[0] = map.get(i).get(0);
-						result[1] = map.get(i).get(1);
-						break;
-					}
-					else
-						continue;
-				}
-				else {
-					result[0] = map.get(i).get(0);
-					result[1] = map.get(target-i).get(0);
-					break;
-				}
-			}	
-		}
-		return result;
-    }
-    
+public class 1TwoSum {        
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         if(nums == null || nums.length < 2)
--- 4MedianOfTwoSortedArrays.java
+++ 4MedianOfTwoSortedArrays.java
@@ -7,7 +7,62 @@ The overall run time complexity should be O(log (m+n)).
 */
 
 public class Solution {
+
+	/*
+		This is not a O(log (m + n)) method, but got accepted by oj and beat 45 %.
+		This is a O(m+n) method. It's like merge 2 sorted array, keep 1 pointer at each array, and always move the 
+		one with smaller num value forward, in the mean time count the value that we passed, until we reached target count, which is
+		(length1 + length2) /2
+	*/
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
+        if (nums1 == null && nums2 == null) return 0;
+        if (nums1 == null) {
+        	if (nums2.length % 2 == 1) return nums2[nums2.length / 2];
+        	else {
+        		return (nums2[nums2.length / 2] + nums2[nums2.length / 2 - 1])/2;
+        	}
+        }
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        int i = 0, j = 0, count = 0;
+        boolean n1 = false;
+        int pre = 0, cur = 0;
+
+        while (i < l1 || j < l2) {
+        	pre = cur;
+        	if (i == l1) {
+        		cur = nums2[j];
+        		j++;
+        		n1 = false;
+        	}
+        	else if (j == l2) {
+        		cur = nums1[i];
+        		i++;
+        		n1 = true;
+        	}
+        	else if (nums1[i] <= nums2[j]) {
+        		cur = nums1[i];
+        		i++;
+        		n1 = true;
+        	}
+        	else {
+        		cur = nums2[j];
+        		j++;
+        		n1 = false;
+        	}
+        	count ++;
+        // 	System.out.println("count = " + count );
+        // 	System.out.println("cur = " + cur );
+        // 	System.out.println("pre = " + pre );
+        	if (count == (l1 + l2)/2 + 1){
+        		if ((l1 + l2) % 2 == 1) {
+        			return cur;
+        		}
+        		else {
+        			return (double)(pre + cur)/2;
+        		}
+        	}
+        }
+        return 0;
     }
 }
